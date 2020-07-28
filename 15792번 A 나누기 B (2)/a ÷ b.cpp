604bffdfd0af898603a64149702fc6ac170ddc91
kkalkkkal <63850490+kkalkkkal@users.noreply.github.com>
2020-07-28 22:56:17 +0900

    Create a ÷ b.cpp

---
+++ 15792번 A 나누기 B (2)/a ÷ b.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <list>
+#include <string>
+
+using namespace std;
+#define MAXN 10000
+
+int A, B;
+long double ans;
+int N, M;
+
+int calculate(int a, int b) {
+
+   A = (10 * a) % b;
+   return (10 * a) / b;
+    
+};
+
+
+int main(void)
+{
+
+
+    cin >> A >> B;
+    
+    ans = 0;
+
+    ans = A / B; // 몫만 우선 구함.
+
+    printf("%d", A / B);
+    printf(".");
+    
+    A = A % B; // 나머지를 A에 넣음.
+
+    for (int i = 1; i < 2000; i++) // 소수점 아래 계산
+    {
+        printf("%d", calculate(A, B));
+    }
+
+    printf("\n");
+    
+
+
+
+    return 0;
+}
